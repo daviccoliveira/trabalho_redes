c8a1df4d56ac7ab8974416bb8e7bfddad390d7fb
Cleber Couto <clebercoutof@gmail.com>
2018-08-13 11:48:15 -0300

    versao final

---
+++ ethernet.ino
@@ -0,0 +1,121 @@
+
+
+#include <SPI.h> 
+#include <Ethernet.h>
+#include "Mudbus.h"
+
+Mudbus Mb;
+//Function codes 1(read coils), 3(read registers), 5(write coil), 6(write register)
+//signed int Mb.R[0 to 125] and bool Mb.C[0 to 128] MB_N_R MB_N_C
+//Port 502 (defined in Mudbus.h) MB_PORT
+
+int botao = 8;
+int LED1 = 7; //Definição das portas dos LEDs
+int LED2 = 6;
+int LED3 = 5;
+int setpoint;
+
+
+void setup(){
+  uint8_t mac[]     = { 0x90, 0xA2, 0xDA, 0x00, 0x51, 0x06 };
+  uint8_t ip[]      = { 192, 168, 1, 10 };
+  uint8_t gateway[] = { 192, 168, 1, 1 };
+  uint8_t subnet[]  = { 255, 255, 255, 0 };
+  Ethernet.begin(mac, ip,gateway, subnet);
+  //With the last update of Industrial Shields boards it's not necessary to use function pinMode() 
+
+  
+  pinMode (LED1, OUTPUT); //Modos de operação dos LEDs (saída)
+  pinMode (LED2, OUTPUT);
+  pinMode (LED3, OUTPUT);
+  pinMode (botao, INPUT);
+}
+
+void loop(){
+  Mb.Run(); //Update the values of Mb.R and Mb.C every loop cycle
+
+  switch(Mb.R[0]) {
+   case 0:
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      Mb.R[1] = 0;
+      Mb.R[2] = 0;
+      Mb.R[3] = 0;
+   break;
+   case 1:
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      Mb.R[1] = 1;
+      Mb.R[2] = 0;
+      Mb.R[3] = 0;
+   break;
+   case 2:
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      Mb.R[1] = 0;
+      Mb.R[2] = 1;
+      Mb.R[3] = 0;
+   break;
+    case 3:
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      Mb.R[1] = 1;
+      Mb.R[2] = 1;
+      Mb.R[3] = 0;
+   break;
+    case 4:
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      Mb.R[1] = 0;
+      Mb.R[2] = 0;
+      Mb.R[3] = 1;
+   break;
+    case 5:
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      Mb.R[1] = 1;
+      Mb.R[2] = 0;
+      Mb.R[3] = 1;
+   break;
+    case 6:
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      Mb.R[1] = 0;
+      Mb.R[2] = 1;
+      Mb.R[3] = 1;
+   break; 
+    case 7:
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+       Mb.R[1] = 1;
+       Mb.R[2] = 1;
+       Mb.R[3] = 1;
+   break;
+}
+  
+ 
+  Mb.R[4] = analogRead(A0);
+  setpoint = Mb.R[5];
+  
+  Mb.R[6] = Mb.R[4] + setpoint;
+
+//  if( digitalRead(botao)){
+//
+//    Mb.R[7] = 1;
+//  }
+//  else{
+//    Mb.R[7] = 0;
+//  }
+  Mb.R[7] = digitalRead(botao);
+  
+
+  }
+
---
+++ rtu.ino
@@ -0,0 +1,127 @@
+#include <SimpleModbusSlave.h>
+
+int LED1 = 8; //Definição das portas dos LEDs
+int LED2 = 6;
+int LED3 = 4;
+int botao = 2;
+
+
+
+enum //Enumera as variáveis  que serão utilizadas para comunicação com o Elipse E3
+{
+  BINARIO, // recebe o valor em decimal para ser convertido em binario
+  LED_1, 
+  LED_2, 
+  LED_3,
+  POTENCIOMETRO, //Recebe o valor do sensor LDR; primeiro componente que o E3 irá "reconhecer"(N4 = 01)
+  SETPOINT, //Recebe o valor decimal; (N4 = 02)
+  SETPOT,
+  BOTAO,  
+  HOLDING_REGS_SIZE //Identifica a quantidade de holdingRegs que estão sendo utilizados no programa.
+};
+
+unsigned int holdingRegs[HOLDING_REGS_SIZE]; //Variável criada para manipulação dos registradores que foram criados. 
+
+int num = 0;
+int setpoint;
+
+void setup() {
+  
+modbus_configure(&Serial, 9600, SERIAL_8N2, 1, 2, HOLDING_REGS_SIZE, holdingRegs); //Determina os parâmetros necessários para estabelecer a conexão via comunicação serial utilizando MODBUS.
+//9600 = velocidade da transmissão dos dados; SERIAL_8N2 = formato do pacote utilizado no MODBUS; 1 = identificação do escravo.
+
+modbus_update_comms(9600, SERIAL_8N2, 1); //Função também responsável pela comunicação via MODBUS; 
+
+pinMode (LED1, OUTPUT); //Modos de operação dos LEDs (saída)
+pinMode (LED2, OUTPUT);
+pinMode (LED3, OUTPUT);
+pinMode (botao, INPUT);
+}
+
+
+void loop() {
+  
+ modbus_update(); // Função utilizada para a atulização dos valores dos registradores declarados (V_LDR, V_LED...)
+ 
+ holdingRegs[POTENCIOMETRO] = analogRead(A0); //Lê-se a informação presente na porta analógica A0 (Sensor LDR)
+ setpoint = holdingRegs[SETPOINT];
+ holdingRegs[SETPOT] = setpoint + analogRead(A0);
+ holdingRegs[BOTAO] = digitalRead(botao);
+
+  switch(holdingRegs[BINARIO]) {
+   case 0:
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      holdingRegs[LED_1] = 0;
+      holdingRegs[LED_2] = 0;
+      holdingRegs[LED_3] = 0;
+   break;
+   case 1:
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      holdingRegs[LED_1] = 1;
+      holdingRegs[LED_2] = 0;
+      holdingRegs[LED_3] = 0;
+   break;
+   case 2:
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      holdingRegs[LED_1] = 0;
+      holdingRegs[LED_2] = 1;
+      holdingRegs[LED_3] = 0;
+   break;
+    case 3:
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      holdingRegs[LED_1] = 1;
+      holdingRegs[LED_2] = 1;
+      holdingRegs[LED_3] = 0;
+   break;
+    case 4:
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      holdingRegs[LED_1] = 0;
+      holdingRegs[LED_2] = 0;
+      holdingRegs[LED_3] = 1;
+   break;
+    case 5:
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      holdingRegs[LED_1] = 1;
+      holdingRegs[LED_2] = 0;
+      holdingRegs[LED_3] = 1;
+   break;
+    case 6:
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      holdingRegs[LED_1] = 0;
+      holdingRegs[LED_2] = 1;
+      holdingRegs[LED_3] = 1;
+   break; 
+    case 7:
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      holdingRegs[LED_1] = 1;
+      holdingRegs[LED_2] = 1;
+      holdingRegs[LED_3] = 1;
+   break;
+}
+  
+ 
+ 
+ }
+
+
+
+
+
+ 
+
